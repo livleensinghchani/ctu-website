92e580c0a2c4e8bfe3383449b3b2cce5f45f3eea
livleensinghchani@gmail.com <livleensinghchani@gmail.com>
2024-02-22 23:01:02 +0530

    Push

--- database.php
+++ database.php
@@ -1,3 +1,17 @@
 <?php
-    
+    $db_server = 'localhost';
+    $db_name = 'ctu';
+    $db_user = 'root';
+    $db_password = 'a';
+
+    $connection = '';
+    try{
+        $connection = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+    } catch (mysqli_sql_exception) {
+        echo("Connection Failed");
+    }
+
+    if($connection) {
+        echo("You are Connected");
+    }
 ?>

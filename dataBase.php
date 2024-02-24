3ee1d032ba79a058886a3ab6cfbb764bace9f0ca
livleensinghchani@gmail.com <livleensinghchani@gmail.com>
2024-02-24 22:54:28 +0530

    Connection to db

---
+++ dataBase.php
@@ -0,0 +1,28 @@
+<?php 
+        // To make php print debug in console
+    function echoToConsole($dataOut) {
+            // check in console!
+        echo '<script> console.log("'.$dataOut.'");</script>';
+    }
+        
+        // db credentials
+    $dbUserName = "raoot";
+    $dbUserPass = "";
+    $dbServer = "localhost";
+    $dbName = "ctu";
+    $dbConnect = '';
+
+    try {
+        $dbConnect = mysqli_connect($dbServer, $dbUserName, $dbUserPass, $dbName);
+    } catch (mysqli_sql_exception) {
+        echoToConsole("Invalid Connection Details");
+    }
+
+       
+    if($dbConnect) {  // check if connection success
+        echoToConsole("Connection Success"); 
+    } else {
+        echoToConsole("Connection Fail");
+    }
+
+?>

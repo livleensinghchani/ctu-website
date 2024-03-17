a147ae08b0caec85402002682cb1601ca1bd851b
livleensinghchani@gmail.com <livleensinghchani@gmail.com>
2024-03-17 20:08:15 +0530

    LOG

--- index.php
+++ index.php
@@ -1,5 +1,51 @@
 <?php
     include("PHP\dataBase.php");
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $type = $_POST['userType'];
+        if(empty($username) || empty($password) || empty($type)) {
+            echo "EMPTY";
+        } else {
+
+            //Sanitization of Input
+            $username = filter_var($username, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $password = filter_var($password, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $type = filter_var($type, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            switch ($type) {
+                case 'Admin':
+                    //Admin Login
+                    $sql = "SELECT * FROM admin WHERE username = $username";
+                    break;
+                case 'Student':
+                    //Student Login
+                    $sql = "SELECT * FROM student WHERE username = $username";
+                    break;
+                case 'Staff':
+                    //Staff Login
+                    $sql = "SELECT * FROM staff WHERE username = $username";
+                    break;
+                default:
+                    //invalid Type
+
+                    break;
+            }
+            try {
+                $result = $dbConnect->query($sql);
+                    
+                $row = $result->fetch_assoc();
+                if($username == $row['username'] && $password == $row['password']) {
+                    echo "VALID LOGIN";
+                } else {
+                    echo 'INVALID LOGIN';
+                }
+            } catch (mysqli_sql_exception) {
+                echo 'INVALID LOGIN';
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +60,19 @@
     <script src="JS/script.js" defer></script>
 </head>
 <body>
-    
+    <div class="MainWrapper">
+        <div class="LoginWrapper">
+            <form action="index.php" method="post">
+                <select name="userType" id="userType">
+                    <option value="Admin">Admin</option>
+                    <option value="Student">Student</option>
+                    <option value="Staff">Staff</option>
+                </select>
+                UserID<input type="text" name="username">
+                Password<input type="text" name="password">
+                <button type="submit">LOGIN</button>
+            </form>
+        </div>
+    </div>
 </body>
-</html>
+</html> 

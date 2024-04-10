ee38da1ddb22a55fe8efef73c445665853db7c59
livleensinghchani@gmail.com <livleensinghchani@gmail.com>
2024-04-10 19:01:10 +0530

    FIX

---
+++ app/sql/query.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+program ( id, name, specialization, type, semester_size, school) 
+VALUES(
+    'BCA', 
+    'Bachelor of Computer Application',
+    'Normal',
+    'UG',
+    6,
+    'SPET'
+    );

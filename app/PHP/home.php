cd7eb63bcc534c5cec40a01aafc2b1ccc01a6cfe
livleensinghchani@gmail.com <livleensinghchani@gmail.com>
2024-04-11 15:33:43 +0530

    New

--- app/PHP/home.php
+++ app/PHP/home.php
@@ -10,6 +10,43 @@
     $userData = $_SESSION['userData'];
 ?>
 
+<!-- DB Link -->
+
+<?php 
+    if($_SESSION['userType'] == 'staff') {
+        //TODO: Staff Panel 
+        $username = $_SESSION['username'];
+        $sql = "SELECT * FROM class WHERE mentor = $username;";
+
+        try {
+            /** @var mysqli $dbConnect */
+            $classAssigned = $dbConnect->query($sql);
+
+            if($classAssigned->num_rows > 0) {
+                while($row = $classAssigned->fetch_assoc()) {
+                    echo "Program: " . $row['program'] . $row['section'];
+                }
+            } else {
+                echo "No Class Assigned";
+            }
+
+        } catch(mysqli_sql_exception) {
+            $outMessage = 'Invalid Password';
+        }
+        
+    } else if($_SESSION['userType'] == 'student') {
+        if(!$userData['reporting_form']) {
+            echo('<a href="reportingForm.php">Reporting From</a>');
+        } else {
+            echo('DONE');
+        }
+    }
+
+    if(isset($outMessage)) {
+        echoToConsole($outMessage);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,40 +66,7 @@
     <div><h1><a href="logout.php">LogOut</a></h1></div>
     <h2>INBOX</h2>
     <h3>Reporting Status 
-        <?php 
-            if($_SESSION['userType'] == 'staff') {
-                //TODO: Staff Panel 
-                $username = $_SESSION['username'];
-                $sql = "SELECT * FROM class WHERE mentor = $username;";
-
-                try {
-                    /** @var mysqli $dbConnect */
-                    $classAssigned = $dbConnect->query($sql);
-
-                    if($classAssigned->num_rows > 0) {
-                        while($row = $classAssigned->fetch_assoc()) {
-                            echo "Program: " . $row['program'] . $row['section'];
-                        }
-                    } else {
-                        echo "No Class Assigned";
-                    }
-
-                } catch(mysqli_sql_exception) {
-                    $outMessage = 'Invalid Password';
-                }
-                
-            } else if($_SESSION['userType'] == 'student') {
-                if(!$userData['reporting_form']) {
-                    echo('<a href="reportingForm.php">Reporting From</a>');
-                } else {
-                    echo('DONE');
-                }
-            }
-
-            if(isset($outMessage)) {
-                echoToConsole($outMessage);
-            }
-        ?>
+        
     </h3>
 </body>
 </html>

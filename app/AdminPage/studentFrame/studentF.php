bf6d9f0143e0bc84ea95f55acd24f05bc5be43ce
livleensinghchani@gmail.com <livleensinghchani@gmail.com>
2024-04-17 12:26:34 +0530

    Change

---
+++ app/AdminPage/studentFrame/studentF.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CTU</title>
+</head>
+<body>
+    <button type="button">Add New Student</button>
+    <button type="button">Find A Student</button>
+</body>
+</html>
